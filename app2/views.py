db08f6ca34468fd5547593cf967670350f100e0e
Lakkireddy Madhava <lakkireddy7111@gmail.com>
2022-12-26 16:01:51 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def madhav1(request):
+    return HttpResponse('<h1><b>Jspiders coaching center</h1></b>')
